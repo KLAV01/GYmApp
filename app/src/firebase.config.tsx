8606c21f5a708399b03f167675ad91a0ec676766
Kaique Vaz <klav@MacBook-Pro-de-Kaique.local>
2024-06-10 18:25:49 -0300

    update

--- app/src/firebase.config.tsx
+++ app/src/firebase.config.tsx
@@ -1,14 +1,15 @@
+
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCKAvQ3Ftm_tA9F4TQL0A6goqnRw3DzUsA",
-  authDomain: "gymapp-klav.firebaseapp.com",
-  projectId: "gymapp-klav",
-  storageBucket: "gymapp-klav.appspot.com",
-  messagingSenderId: "899395829593",
-  appId: "1:899395829593:web:b3666ff416111b82821648"
+  apiKey: "AIzaSyDgXnjNRXmuPmEfk5v4iKiAnJS6Ea5o60o",
+  authDomain: "app-gym-e0341.firebaseapp.com",
+  projectId: "app-gym-e0341",
+  storageBucket: "app-gym-e0341.appspot.com",
+  messagingSenderId: "155737682936",
+  appId: "1:155737682936:web:dae5cb1d0a3a5ae5865c0d"
 };
 
 export const firebase = initializeApp(firebaseConfig);
